553e7e834a3157304747bf8f31238f46c764aedf
Francois Bernier <frankbernier@gmail.com>
2017-10-22 15:25:51 -0400

    Remove circular dependency requirement in tests

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,7 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'tomlrb'
 
-require 'minitest/spec'
 require 'minitest/autorun'
 require "minitest/reporters"
 Minitest::Reporters.use!
