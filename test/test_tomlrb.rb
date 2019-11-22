5c421c7f34e85648571a21e6424ae7b766b68065
Ryan Davis <ryand-ruby@zenspider.com>
2019-11-21 17:53:29 -0800

    Fixed warning from (intentional) float overflow.
    The point was to lex/parse the underscores, not to create
    Float::INFINITY so I just chopped it down.
    
    Signed-off-by: Ryan Davis <zenspider@chef.io>

--- test/test_tomlrb.rb
+++ test/test_tomlrb.rb
@@ -54,7 +54,7 @@ class TomlExamples
     {"fractional"=>{"key1"=>1.0, "key2"=>3.1415, "key3"=>-0.01},
      "exponent"=>{"key1"=>5.0e+22, "key2"=>1000000.0, "key3"=>-0.02},
      "both"=>{"key"=>6.626e-34},
-     "underscores"=>{"key1"=>9224617.445991227, "key2"=>1e1_000}},
+     "underscores"=>{"key1"=>9224617.445991227, "key2"=>1e1_0_0}},
     "boolean"=>{"True"=>true, "False"=>false},
     "datetime"=>{"key1"=>Time.utc(1979, 05, 27, 07, 32, 0), "key2"=>Time.new(1979, 05, 27, 00, 32, 0, '-07:00'), "key3"=>Time.new(1979, 05, 27, 00, 32, 0.999999, '-07:00'), "key4"=>Time.utc(1979, 05, 27, 0, 32, 0), "key5"=>Time.utc(1979, 05, 27, 0, 0, 0)}, "array"=>{"key1"=>[1, 2, 3], "key2"=>["red", "yellow", "green"], "key3"=>[[1, 2], [3, 4, 5]], "key4"=>[[1, 2], ["a", "b", "c"]], "key5"=>[1, 2, 3], "key6"=>[1, 2]},
     "products"=>[{"name"=>"Hammer", "sku"=>738594937}, {}, {"name"=>"Nail", "sku"=>284758393, "color"=>"gray"}],
