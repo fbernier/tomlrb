2d9385566a0de1cae426cc7dc598039bc30c5ed1
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2022-01-02 07:28:52 +0900

    Parse toml-test's JSON

--- test/test_compliance.rb
+++ test/test_compliance.rb
@@ -25,55 +25,111 @@ INT_KEY = %w[
 ]
 
 describe Tomlrb::Parser do
-  tests_dir = File.join(__dir__, '../toml-spec-tests')
-  Pathname.glob("#{tests_dir}/values/*.toml").each do |toml_path|
-    toml_path = toml_path.expand_path
-    local_path = toml_path.relative_path_from(File.join(__dir__, ".."))
+  tests_dirs = [
+    File.join(__dir__, '../toml-spec-tests'),
+    File.join(__dir__, '../toml-test/tests')
+  ]
+  tests_dirs.each do |tests_dir|
+    Pathname.glob("#{tests_dir}/{values,valid}/**/*.toml").each do |toml_path|
+      toml_path = toml_path.expand_path
+      local_path = toml_path.relative_path_from(File.join(__dir__, ".."))
 
-    it "parses #{local_path}" do
-      actual = Tomlrb.load_file(toml_path.to_path)
-      json_path = toml_path.sub_ext('.json')
-      yaml_path = toml_path.sub_ext('.yaml')
-      expected =
-        if json_path.exist?
-          load_json(json_path)
-        elsif yaml_path.exist?
-          load_yaml(yaml_path)
-        end
-      _(actual).must_equal expected
+      it "parses #{local_path}" do
+        actual = Tomlrb.load_file(toml_path.to_path)
+        json_path = toml_path.sub_ext('.json')
+        yaml_path = toml_path.sub_ext('.yaml')
+        expected =
+          if json_path.exist?
+            load_json(json_path)
+          elsif yaml_path.exist?
+            load_yaml(yaml_path)
+          end
+        _(actual).must_equal expected
+      end
+    end
+
+    Pathname.glob("#{tests_dir}/{errors,invalid}/**/*.toml").each do |toml_path|
+      toml_path = toml_path.expand_path
+      local_path = toml_path.relative_path_from(File.join(__dir__, ".."))
+
+      it "raises an error on parsing #{local_path}" do
+        _{ Tomlrb.load_file(toml_path.to_path) }.must_raise Tomlrb::ParseError, RangeError, ArgumentError
+      end
     end
   end
+end
 
-  Pathname.glob("#{tests_dir}/errors/*.toml").each do |toml_path|
-    toml_path = toml_path.expand_path
-    local_path = toml_path.relative_path_from(File.join(__dir__, ".."))
-
-    it "raises an error on parsing #{local_path}" do
-      _{ Tomlrb.load_file(toml_path.to_path) }.must_raise Tomlrb::ParseError, RangeError
+def process_json_leaf(node)
+  v = node['value']
+  case node['type']
+  when 'string'
+    v
+  when 'integer'
+    v.to_i
+  when 'float'
+    case v
+    when 'inf', '+inf'
+      Float::INFINITY
+    when '-inf'
+      -Float::INFINITY
+    when 'nan'
+      Float::NAN
+    else
+      v.to_f
     end
+  when 'bool'
+    case v
+    when 'true'
+      true
+    when 'false'
+      false
+    end
+  when 'datetime-local'
+    date, time = v.split(/[t ]/i)
+    year, month, day = date.split('-')
+    hour, min, sec = time.split(':')
+    Tomlrb::LocalDateTime.new(year, month, day, hour, min, sec.to_f)
+  when 'date', 'date-local'
+    Tomlrb::LocalDate.new(*v.split('-'))
+  when 'time', 'time-local'
+    hour, min, sec = v.split(':')
+    Tomlrb::LocalTime.new(hour, min, sec.to_f)
+  when 'datetime'
+    if v.match?(/\.\d+/)
+      date, time = v.split("T")
+      year, month, day = date.split("-")
+      hour, minute, second_and_zone = time.split(":", 3)
+      second, frac_and_zone = second_and_zone.split(".")
+      md = frac_and_zone.match(/(?<frac>\d+)(?<zone>.*)/)
+      frac = md[:frac]
+      zone = md[:zone]
+      Time.new(year, month, day, hour, minute, "#{second}.#{frac}".to_f, zone)
+    else
+      Time.parse(v)
+    end
+  end
+end
+
+def process_json(node)
+  case node
+  when Hash
+    if node["type"]
+      process_json_leaf(node)
+    else
+      node.each_with_object({}) {|(key, value), table|
+        table[key] = process_json(value)
+      }
+    end
+  when Array
+    node.map {|entry| process_json(entry)}
+  else
+    node
   end
 end
 
 def load_json(path)
   json = JSON.load(path.open)
-  json.each_with_object({}) {|(key, value), table|
-    v = value['value']
-    table[key] =
-      case value['type']
-      when 'integer'
-        v.to_i
-      when 'datetime-local'
-        date, time = v.split(/[t ]/i)
-        year, month, day = date.split('-')
-        hour, min, sec = time.split(':')
-        Tomlrb::LocalDateTime.new(year, month, day, hour, min, sec.to_f)
-      when 'date'
-        Tomlrb::LocalDate.new(*v.split('-'))
-      when 'time'
-        hour, min, sec = v.split(':')
-        Tomlrb::LocalTime.new(hour, min, sec.to_f)
-      end
-  }
+  process_json(json)
 end
 
 def load_yaml(path)
